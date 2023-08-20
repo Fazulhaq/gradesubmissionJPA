3ec4959301e45ea771c752ccfdfc6c27e74f3c0a
Fazulhaq Amin <128462337+Fazulhaq@users.noreply.github.com>
2023-08-20 15:24:04 +0430

    Grade APIs completed

--- src/main/java/com/mcit/gradesubmisionjpa/repository/GradeRepository.java
+++ src/main/java/com/mcit/gradesubmisionjpa/repository/GradeRepository.java
@@ -1,10 +1,16 @@
 package com.mcit.gradesubmisionjpa.repository;
 
 import com.mcit.gradesubmisionjpa.entity.Grade;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface GradeRepository extends CrudRepository<Grade, Long> {
         Optional<Grade> findByStudentIdAndCourseId(Long StudentId, Long CourseId);
+        @Transactional
+        void deleteByStudentIdAndCourseId(Long StudentId, Long CourseId);
+        List<Grade> findByStudentId(Long StudentId);
+        List<Grade> findByCourseId(Long CourseId);
 }
